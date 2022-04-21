bb3615029d9baa7f9b15047e1632c1f75df62b60
dahye lim <hapwoo@gmail.com>
2022-04-21 09:21:07 +0900

    Initial Commit

---
+++ MyAssets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAssets
+//
+//  Created by 임다혜 on 2022/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by 임다혜 on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
